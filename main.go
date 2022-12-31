c0d13cdb36181f465248989e61e0b03d0b667f4d
nghe.huavan <nghe.huavan@gmail.com>
2022-12-31 10:30:31 +0700

    .

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		paramPairs := c.Request.URL.Query()
+		fmt.Printf("paramPairs = %v\n", paramPairs)
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
