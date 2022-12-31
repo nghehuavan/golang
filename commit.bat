042954c2e62f1bb1612a6353801c5f472b95b66c
nghe.huavan <nghe.huavan@gmail.com>
2022-12-31 10:32:56 +0700

    [--]

---
+++ commit.bat
@@ -0,0 +1,26 @@
+@echo off
+:CD ..
+set excludeFileName=exclude.lst
+set excludeFileFullPath=%CD%\%excludeFileName%
+:begin
+@echo "@@ Commit And Push All File @@"
+SET /P sComment=Please comment for commit (empty is default):
+
+git add --all
+
+@IF %sComment%.==. GOTO defaultComment
+GOTO userComment
+
+:defaultComment
+git commit -m "[--]"
+goto push
+
+:userComment
+git commit -m "%sComment%"
+
+:push
+git.exe push --all --thin --progress "origin"
+:end
+pause
+cls 
+goto begin
---
+++ reset.bat
@@ -0,0 +1,14 @@
+@echo off
+:CD ..
+@echo "@@ Reset And Pull All File @@"
+SET /P sComment= Enter for PULL ?
+
+@echo "@@ Revert File .... @@"
+git reset --hard
+@echo "@@ Pull File .... @@"
+git.exe pull -v --progress "origin"
+
+SET /P sComment= Enter for CLEAN-UP ?
+git clean -d -f
+git clean -d -f -X
+pause
